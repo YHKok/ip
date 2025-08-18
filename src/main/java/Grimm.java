3659b39c0bf2355c64ed2683e3818007b53bec58
YHKok <yonghsienkok.17@gmail.com>
2025-08-18 22:19:49 +0800

    Echo

--- src/main/java/Grimm.java
+++ src/main/java/Grimm.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Grimm {
     public static void main(String[] args) {
-        String logo = "Hello, I'm Grimm\nWhat can I do for you?\nBye. Hope to see you again soon!";
+        String logo = "Hello, I'm Grimm\nWhat can I do for you?\n";
         System.out.println(logo);
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        while (!input.toLowerCase().equals("bye")) {
+            if (input.toLowerCase().equals("dream"))
+                System.out.println("The child......");
+            else if (input.toLowerCase().equals("lore"))
+                System.out.println("The ritual......");
+            else
+                System.out.println(input);
+            input = scan.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
