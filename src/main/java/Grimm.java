88c23a5ba2ff8e1227b02956e54aa06549302500
YHKok <yonghsienkok.17@gmail.com>
2025-08-18 22:05:51 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Grimm.java
@@ -0,0 +1,6 @@
+public class Grimm {
+    public static void main(String[] args) {
+        String logo = "Hello, I'm Grimm\nWhat can I do for you?\nBye. Hope to see you again soon!";
+        System.out.println(logo);
+    }
+}
