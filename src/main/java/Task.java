20a45b703293024fc534deefa20dc52718492b48
YHKok <yonghsienkok.17@gmail.com>
2025-08-27 17:01:45 +0800

    Task.java: rename Tasks.java and add helpers and add constructor
    Overloaded constructors to support file upload
    
    Added helper methods
    
    Renamed class from Tasks to Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,41 @@
+public class Task {
+    private boolean isMarked = false;
+    private String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public Task(String name, boolean mark) {
+        this.name = name;
+        if (mark) {
+            this.mark();
+        }
+    }
+
+    public void mark() {
+        this.isMarked = true;
+    }
+
+    public void unmark() {
+        this.isMarked = false;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean getMark() {
+        return this.isMarked;
+    }
+
+    @Override
+    public String toString() {
+        if (this.isMarked) {
+            return "[X] " + this.getName();
+        } else {
+            return "[ ] " + this.getName();
+        }
+    }
+
+}
