7a8c4b4f20bee29f5e91318a21b41353dc4551bd
YHKok <yonghsienkok.17@gmail.com>
2025-08-29 00:13:33 +0800

    Update Event.java

--- src/main/java/grimm/model/Event.java
+++ src/main/java/grimm/model/Event.java
@@ -5,18 +5,43 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+/**
+ * Represents an Event task with a start and end date.
+ * <p>
+ * The Event has a description, start date and end date. It extends the Task class.
+ * Functionality to format and display start date and end date.
+ * </p>
+ */
 public class Event extends Task {
     private String startDate;
     private String endDate;
     private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("MM/dd/yyyy HHmm");
     private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("d MMMM yyyy, h:mm a").withLocale(Locale.forLanguageTag("en-SG"));
 
-
+    /**
+     * Constructs an Event task with a given description, start date, and end date.
+     * <p>
+     * The Event will be unmarked.
+     * </p>
+     *
+     * @param name      The description of the event.
+     * @param startDate The start date of the event in "MM/dd/yyyy HHmm" format.
+     * @param endDate   The end date of the event in "MM/dd/yyyy HHmm" format.
+     */
     public Event(String name, String startDate, String endDate) {
         super(name);
         this.startDate = startDate;
         this.endDate = endDate;
     }
+
+    /**
+     * Constructs an Event task with a given description, mark status, start date, and end date.
+     *
+     * @param name      The description of the event.
+     * @param mark      The mark status of the event (true if marked, false if unmarked).
+     * @param startDate The start date of the event in "MM/dd/yyyy HHmm" format.
+     * @param endDate   The end date of the event in "MM/dd/yyyy HHmm" format.
+     */
 
     public Event(String name, boolean mark, String startDate, String endDate) {
         super(name, mark);
@@ -32,6 +57,12 @@ public class Event extends Task {
         return this.endDate;
     }
 
+    /**
+     * Formats the start and end dates of the event from "MM/dd/yyyy HHmm" to "d MMMM yyyy, h:mm a".
+     * If the date format is invalid, it returns the raw start and end dates.
+     *
+     * @return A formatted string with the event's start and end date-times, or the original string if parsing fails.
+     */
     public String formatDateTime() {
         try {
 
