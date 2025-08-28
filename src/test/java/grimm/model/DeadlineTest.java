28303c03764fb6dfd3867ff594438a8c117b4913
YHKok <yonghsienkok.17@gmail.com>
2025-08-29 00:29:24 +0800

    Update DeadlineTest.java

--- src/test/java/grimm/model/DeadlineTest.java
+++ src/test/java/grimm/model/DeadlineTest.java
@@ -7,19 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DeadlineTest {
 
     @Test
-    void deadline_validFormat() {
+    public void formatDate_validDateFormat_correctlyFormatted() {
         Deadline d = new Deadline("submit report", "12/02/2002");
         assertEquals("2 December 2002", d.formatDate());
     }
 
     @Test
-    void deadline_unMarked() {
+    public void toString_unMarked_correctFormat() {
         Deadline d = new Deadline("submit report", "12/02/2002");
         assertEquals("[D][ ] submit report (by: 2 December 2002)", d.toString());
     }
 
     @Test
-    void deadlline_marked() {
+    public void toString_marked_correctFormat() {
         Deadline d = new Deadline("submit report", true, "12/02/2002");
         assertEquals("[D][X] submit report (by: 2 December 2002)", d.toString());
     }
