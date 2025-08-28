f8d6b4c2df927bbfcf1a05f4189a0419666d5186
YHKok <yonghsienkok.17@gmail.com>
2025-08-29 00:16:12 +0800

    Update Command.java
    Converted the class to an enum.
    
    Added a convert() method.

--- src/main/java/grimm/parse/Command.java
+++ src/main/java/grimm/parse/Command.java
@@ -1,21 +1,35 @@
 package grimm.parse;
 
-public class Command {
-    public static final String BYE = "bye";
-    public static final String LIST = "list";
-    public static final String MARK = "mark";
-    public static final String UNMARK = "unmark";
-    public static final String TODO = "todo";
-    public static final String DEADLINE = "deadline";
-    public static final String EVENT = "event";
-    public static final String DELETE = "delete";
-    public static final String UNKNOWN = "";
+/**
+ * Represents a command in the Grimm application.
+ * <p>
+ * The Command enum encapsulates various commands such as "bye", "list", "mark",
+ * "unmark", "todo", "deadline", "event", and "delete". Each command is represented
+ * by a string value and the enum provides functionality to validate commands and
+ * convert input strings into appropriate Command objects.
+ * </p>
+ */
+public enum Command {
+    BYE("bye"),
+    LIST("list"),
+    MARK("mark"),
+    UNMARK("unmark"),
+    TODO ("todo"),
+    DEADLINE("deadline"),
+    EVENT("event"),
+    DELETE("delete"),
+    UNKNOWN("");
     private String command;
 
-    public Command(String command) {
-        if (command == null) {
-            this.command = UNKNOWN;
-        }
+    /**
+     * Constructs a Command object with the specified command string.
+     * <p>
+     * If the provided command is null, it will be initialized to an unknown command.
+     * </p>
+     *
+     * @param command The command string to initialize the Command object with.
+     */
+    Command(String command) {
         this.command = command;
     }
 
@@ -23,8 +37,22 @@ public class Command {
         return this.command;
     }
 
-    public boolean isValid(String command) {
-        return command.equals(BYE) || command.equals(LIST) || command.equals(MARK) || command.equals(UNMARK) || command.equals(TODO) || command.equals(DEADLINE) || command.equals(EVENT) || command.equals(DELETE);
+    /**
+     * Converts a string input into a corresponding Command enum.
+     * <p>
+     * This method will attempt to match the input string with one of the predefined
+     * commands in the enum. If no match is found, it returns UNKNOWN.
+     * </p>
+     *
+     * @param input The string input to be converted into a Command.
+     * @return The corresponding Command enum, or UNKNOWN if no match is found.
+     */
+    public static Command convert(String input) {
+        for (Command command : Command.values()) {
+            if (command.getCommand().equalsIgnoreCase(input)) {
+                return command;
+            }
+        }
+        return UNKNOWN;
     }
-
 }
