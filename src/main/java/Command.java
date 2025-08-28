e6afefdf3a3b90cc2223d99a0a23e6eefcd2c4e1
YHKok <yonghsienkok.17@gmail.com>
2025-08-28 15:07:17 +0800

    Update Command.java

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,12 +1,25 @@
 public class Command {
-    private static final String BYE = "bye";
-    private static final String LIST = "list";
-    private static final String MARK = "mark";
-    private static final String UNMARK = "unmark";
-    private static final String TODO = "todo";
-    private static final String DEADLINE = "deadline";
-    private static final String EVENT = "event";
-    private static final String DELETE = "delete";
+    public static final String BYE = "bye";
+    public static final String LIST = "list";
+    public static final String MARK = "mark";
+    public static final String UNMARK = "unmark";
+    public static final String TODO = "todo";
+    public static final String DEADLINE = "deadline";
+    public static final String EVENT = "event";
+    public static final String DELETE = "delete";
+    public static final String UNKNOWN = "";
+    private String command;
+
+    public Command(String command) {
+        if (command == null) {
+            this.command = UNKNOWN;
+        }
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return this.command;
+    }
 
     public boolean isValid(String command) {
         return command.equals(BYE) || command.equals(LIST) || command.equals(MARK) || command.equals(UNMARK) || command.equals(TODO) || command.equals(DEADLINE) || command.equals(EVENT) || command.equals(DELETE);
