9be6ffb1607136fc4958ea86cbbd8632fa90148a
YHKok <yonghsienkok.17@gmail.com>
2025-08-29 00:13:32 +0800

    Update Task.java

--- src/main/java/grimm/model/Task.java
+++ src/main/java/grimm/model/Task.java
@@ -1,13 +1,37 @@
 package grimm.model;
 
+/**
+ * Represents a generic task with a description and a mark status.
+ * <p>
+ * The task has a description and can be marked as completed or unmarked. It can be used as the base class
+ * for more specific task types, such as ToDo, Deadline, and Event.
+ * </p>
+ */
 public class Task {
     private boolean isMarked = false;
     private String name;
 
+    /**
+     * Constructs a Task with a given description.
+     * <p>
+     * The task will initially be unmarked.
+     * </p>
+     *
+     * @param name The description of the task.
+     */
     public Task(String name) {
         this.name = name;
     }
 
+    /**
+     * Constructs a Task with a given description and mark status.
+     * <p>
+     * If the mark parameter is true, the task will be marked as completed.
+     * </p>
+     *
+     * @param name The description of the task.
+     * @param mark The initial mark status of the task (true for marked, false for unmarked).
+     */
     public Task(String name, boolean mark) {
         this.name = name;
         if (mark) {
