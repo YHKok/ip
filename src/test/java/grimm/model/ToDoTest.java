11f230add2495f831bd7e827ae8ab389fef46b72
YHKok <yonghsienkok.17@gmail.com>
2025-08-29 10:35:56 +0800

    Update ToDoTest.java

--- src/test/java/grimm/model/ToDoTest.java
+++ src/test/java/grimm/model/ToDoTest.java
@@ -1,7 +1,6 @@
 package grimm.model;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,43 +8,43 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ToDoTest {
     @Test
     public void toDo_default_notMarked() {
-        ToDo t = new ToDo("read book");
+        ToDo t = new ToDo("Read book");
         assertFalse(t.getMark(), "New ToDo should not be marked by default");
-        assertEquals("[T][ ] read book", t.toString());
+        assertEquals("[T][ ] Read book", t.toString());
     }
 
     @Test
     public void toDo_markedTrue_constructor() {
-        ToDo t = new ToDo("return book", true);
+        ToDo t = new ToDo("Return book", true);
         assertTrue(t.getMark(), "Constructor with true should mark the task");
-        assertEquals("[T][X] return book", t.toString());
+        assertEquals("[T][X] Return book", t.toString());
     }
 
     @Test
     public void toDo_markedFalse_constructor() {
-        ToDo t = new ToDo("join the Troupe", false);
+        ToDo t = new ToDo("Join the Troupe", false);
         assertFalse(t.getMark(), "Constructor with false should leave task unmarked");
-        assertEquals("[T][ ] join the Troupe", t.toString());
+        assertEquals("[T][ ] Join the Troupe", t.toString());
     }
 
     @Test
     public void toDo_markThenUnmark_taskStatusChanges() {
-        ToDo t = new ToDo("defeat Grimm");
+        ToDo t = new ToDo("Defeat Grimm");
         assertFalse(t.getMark());
 
         t.mark();
         assertTrue(t.getMark());
-        assertEquals("[T][X] defeat Grimm", t.toString());
+        assertEquals("[T][X] Defeat Grimm", t.toString());
 
         t.unmark();
         assertFalse(t.getMark());
-        assertEquals("[T][ ] defeat Grimm", t.toString());
+        assertEquals("[T][ ] Defeat Grimm", t.toString());
     }
 
     @Test
     public void toDo_getName_taskNameReturned() {
-        ToDo t = new ToDo("study CS2103");
-        assertEquals("study CS2103", t.getName());
+        ToDo t = new ToDo("Nurture Grimmchild");
+        assertEquals("Nurture Grimmchild", t.getName());
     }
 
     @Test
